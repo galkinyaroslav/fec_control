983afcf159ef1d843d18e1f1748f8582c6f76c3b
Yaroslav Galkin <galkinyaroslav@gmail.com>
2024-01-24 22:51:04 +0300

    add NWaveForm

--- waveform.py
+++ waveform.py
@@ -1,25 +1,123 @@
 import os
-
+import re
 import numpy as np
 from matplotlib import pyplot as plt
 
 
-# class NWaveForm:
-#     def __init__(self, filename: str):
-#         self.__rms = None
-#         self.__raw_data = raw_data
-#         self.__all_data = self.convert_data_to_int(raw_data)
-#         self.__waveform_data = self.__all_data[:, 5:]
-#
-#     def asd(self):
-#         filename = 'events.lst'
-#         with open(f'{filename}', 'r') as file:
-#             lines = file.readlines()
-#         decimal_values = []
-#         for line in lines:
-#             line = line.strip().split()
-#             decimal_values = ([value.lstrip('0x').encode() for value in line])
-#         print(decimal_values)
+class NWaveForm:
+    def __init__(self, full_filename: str):
+        separators = r'[/.-]'
+        self.__full_filename = full_filename
+        self.__filename = full_filename.split('/')[-1]
+        parameters_list = re.split(separators, full_filename)
+        self.__file_number = parameters_list[3]
+        self.__card_number = parameters_list[4]
+        self.__test_name = parameters_list[2]
+        self.__raw_data = self.get_raw_data()
+        self.__all_data = self.convert_data_to_int()
+        self.__waveform_data = self.__all_data[:, :, 5:]
+        self.__rms = self.get_rms()
+
+
+    @property
+    def all_data(self):
+        return self.__all_data
+    @property
+    def full_filename(self):
+        return self.__full_filename
+
+    @property
+    def filename(self):
+        return self.__filename
+
+    @property
+    def card_number(self):
+        return self.__card_number
+
+    @property
+    def test_name(self):
+        return self.__test_name
+
+    @property
+    def raw_data(self):
+        return self.__raw_data
+
+    @property
+    def rms(self):
+        return self.__rms
+
+    @property
+    def waveform_data(self):
+        return self.__waveform_data
+
+    def get_rms(self):
+        self.__rms = self.__waveform_data.std(axis=(0, 2), ddof=1)
+        return self.__rms
+
+    def get_raw_data(self) -> np.ndarray:
+        with open(self.__full_filename, 'r') as file:
+            lines = file.readlines()
+        data_list = []
+        for line in lines:
+            hex_values = line.strip().split()  # Разбиваем строку по пробелам
+            decimal_values = [int(value, 16) for value in hex_values]  # Преобразуем 16-ричные значения в десятичные
+            data_list.append(decimal_values)
+        return np.array(data_list)
+
+    def check_data(self) -> bool:
+        second_column = self.__all_data[:, :, 1]
+        condition = second_column == 31
+        return np.all(condition)
+
+    def convert_data_to_int(self):
+        # pass
+        result_array_3d = np.zeros((self.__raw_data.shape[0],
+                                    self.__raw_data.shape[1] // 12,
+                                    self.__raw_data.shape[1] // 64 * 3), dtype=np.uint16)
+        for i in range(result_array_3d.shape[0]):
+            for j in range(result_array_3d.shape[1]):
+                for k in range(result_array_3d.shape[2] // 3):
+                    val = self.__raw_data[i, 12 * j + k]
+                    d0 = val & 0x3ff
+                    d1 = (val >> 10) & 0x3ff
+                    d2 = (val >> 20) & 0x3ff
+                    result_array_3d[i, j, 3 * k] = d0
+                    result_array_3d[i, j, 3 * k + 1] = d1
+                    result_array_3d[i, j, 3 * k + 2] = d2
+        return result_array_3d
+
+    def plot_waveform(self):
+        px = 1 / plt.rcParams['figure.dpi']  # pixel in inches
+        path = f'./plots/temp/'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        fig, axs = plt.subplots(8, 8, figsize=(3840 * px, 2160 * px), constrained_layout=True)
+        fig.suptitle(f'WaveForm from file {self.__full_filename}')
+        for row in range(8):
+            for col in range(8):
+                axs[row, col].plot(self.__waveform_data
+                                   .transpose(1, 0, 2)
+                                   .reshape(self.__waveform_data.shape[1], -1)[8 * row + col], 'o', )
+                axs[row, col].set_ylim(0)
+                axs[row, col].set_xlabel('time, us')
+                axs[row, col].set_ylabel('ADC channel')
+                axs[row, col].set_title(f'CH-{8 * row + col} WaveForm')
+        plt.show()
+        # plt.savefig(f'{path}/34-454_waveform_unconnected.png')
+
+    def plot_rms(self):
+        path = f'./plots/temp'
+        if not os.path.exists(path):
+            os.makedirs(path)
+        fig, axs = plt.subplots(1, constrained_layout=True)
+        axs.plot(self.__rms, 'o', )
+        axs.set_ylim(0)
+        axs.set_xlabel('Channel number')
+        axs.set_ylabel('RMS channel')
+        axs.set_title(f'RMS from file {self.__full_filename}')
+        plt.show()
+        # plt.savefig(f'{path}/34-454_rms_unconnected.png')
+
 
 class WaveForm:
     def __init__(self, raw_data: list[bytes],):
@@ -97,47 +195,22 @@ class WaveForm:
         self.__rms = np.std(self.__waveform_data, axis=1, ddof=1)
         return self.__rms
 
-# np.set_printoptions(linewidth=1000, threshold=np.inf)
-
-# def get_waveform_data():
-#     result_array_3d = result_array_3d[:, :, 5:]
-
-
-def test():
-    filename = 'events.lst'
-    with open(f'{filename}', 'r') as file:
-        lines = file.readlines()
-    decimal_values = []
-    for line in lines:
-        line = line.strip().split()
-        decimal_values = ([value.lstrip('0x').encode() for value in line])
-    print(decimal_values)
-    # np.set_printoptions(linewidth=1000, threshold=np.inf)
-
 
 if __name__ == '__main__':
     np.set_printoptions(linewidth=1000, threshold=np.inf)
 
-    filename = 'runs/454/raw/43-454.txt'
-    with open(f'{filename}', 'r') as file:
-        lines = file.readlines()
-    decimal_values = ([value.lstrip('0x').encode() for value in lines[0].strip().split()])
-    # for line in lines:
-    #     line = line.strip().split()
-    #     decimal_values = ([value.lstrip('0x').encode() for value in line])
-    print(decimal_values)
-    a = WaveForm(decimal_values)
-    b = a.all_data
-    c = a.raw_data
-    d = a.waveform_data
-    print(f'{b=}')
-    print(len(b))
-    print(f'{c=}')
-    print(len(c))
+    filename = 'runs/454/raw/3-454.txt'
+    a = NWaveForm(full_filename=filename)
+    print(a.filename)
+    print(a.card_number)
+    print(a.full_filename)
+    print(a.filename)
+    print(a.test_name)
+    print(a.raw_data.shape)
 
-    print(f'{d=}')
-    print(len(d[0]))
-    print('checked: ', a.check_data())
-    a.get_rms()
+    print(a.all_data[4])
+    print(a.check_data())
+    print(a.get_rms())
     a.plot_waveform()
     a.plot_rms()
+
